e3699d2b712cd8441b7dca9cf35ba9d60812f090
Pranith Kodali <mymike456@gmail.com>
2019-10-05 13:51:19 -0400

    calculator

---
+++ PycharmProjects/3/9/git hub/cal.py
@@ -0,0 +1,11 @@
+a=int(input("num1"))
+b=int(input("num2"))
+c=int(input("type 1 for addition type 2 for substraction type 3 for multiplucation type 4 for divison"))
+if c==1:
+    print(a+b)
+if c==2:
+    print(a-b)
+if c==3:
+    print(a*b)
+if c==4:
+    print(a/b)
